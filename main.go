76867f6fd0f25241b142de7af9afa81283776509
sunminJeong <tjsals7825@gmail.com>
2020-11-09 16:48:08 +0900

    ~1.7 condition

--- main.go
+++ main.go
@@ -7,22 +7,42 @@ import (
 
 // go entry function
 func main() {
-	total := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(total)
+	result1 := canIDrinkIf(18)
+	fmt.Println(result1)
+	result2 := canIDrinkSwitch(18)
+	fmt.Println(result2)
 }
 
-//
-func superAdd(numbers ...int) int {
-	total := 0
-	// 배열 Loop 접근 ( index, value 제공함)
-	// 1, range 활용
-	for index, number := range numbers {
-		fmt.Println(index, number)
-		total += number
+// IF
+func canIDrinkIf(age int) bool {
+	// if 문 형식 + Go에서는 if block내 변수 선언이 가능하다!
+	if varInIf := age + 2; varInIf > 18 {
+		fmt.Println(varInIf, "true?")
+		return true
 	}
-	// 2, for문 활용
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(i, numbers[i])
+	return false
+}
+
+// SWITCH
+func canIDrinkSwitch(age int) bool {
+	// switch 문 형식 + Go에서는 switch block내 변수 선언이 가능하다!
+	// switch varInIf2 := age + 2; varInIf2 {
+	// case 10:
+	// 	return false
+	// case 18:
+	// 	fmt.Println(varInIf2, "switch here!")
+	// 	return true
+	// case 20:
+	// 	fmt.Println(varInIf2, "switch here?")
+	// 	return false
+	// }
+	switch {
+	case age+2 < 10:
+		return false
+	case age+2 == 18:
+		return true
+	case age+2 >= 20:
+		return false
 	}
-	return total
+	return false
 }
